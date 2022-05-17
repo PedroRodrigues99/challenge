aed7354272d9bc05fb650a1ed1e1c6bc1c98439b
PedroRodrigues99 <pedro.m.rodri@gmail.com>
2022-05-17 03:49:32 +0100

    routes implemented, delete some files

---
+++ invoice-challenge/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Routes from './route'
+
+
+function App() {
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
---
+++ invoice-challenge/src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import List from './Components/Route/ListsRoute'
+import {BrowserRouter} from 'react-router-dom'
+
+function route() {
+    return (
+        <BrowserRouter>
+        <List/>
+        
+        
+        </BrowserRouter>
+    )
+}
+
+export default route
